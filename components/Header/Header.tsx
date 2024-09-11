fade1e6c60c595512b7f5fbc3e04b0af668a8188
Elmar Wouters <elmar@elmarwouters.nl>
2024-09-11 17:53:46 +0200

    ⚡️ Introduced a new feature! Auth0 and Next-Auth

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,9 +1,12 @@
+import { HeaderLogin } from "@/components/Header/HeaderLogin";
+
 export const Header = () => {
   return (
     <header className="bg-primary text-white py-8 px-4 xl:px-0">
       <div className="container">
         Je ma pelle Header
       </div>
+      <HeaderLogin />
     </header>
-  )
-}
+  );
+};
